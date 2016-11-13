8d2c4d79c0c4a9006f34a6a76cc77e5c8579df89
Jahangir <md.jahangir27@gmail.com>
2016-11-13 20:57:18 +0530

    Exception handling via Either.

--- src/main/scala/Exceptions.scala
+++ src/main/scala/Exceptions.scala
@@ -1,3 +1,5 @@
+import scala.util.Try
+
 /**
   * Created by jahangir on 11/13/16.
   */
@@ -22,15 +24,31 @@ object Exceptions extends App {
   }
 
   /**
-    * Option datatype is one of dealing with exceptions which is used to represent that a function doesn't have an answer always.
+    * Option data type is one of dealing with exceptions which is used to represent that a function doesn't have an answer always.
     */
   def rollNumber(name: String): Option[Student] = studentsData.get(name).map(no => Student(name, no))
 
-  println(rollNumberOld("Aman"))
 
-  println(rollNumber("A"))
+  /**
+    * Either data type is a disjoint union of two types to indicate success or failure where by convention Right is used
+    * for success case and Left is used for failure. It provides more information than Option.
+    * Equivalent of this function is Try which provides either Exception or value.
+    */
+  def rollNo(name: String): Either[Exception, Student] =
+    try Right(rollNumberOld(name))
+    catch { case e: Exception => Left(e)}
 
-  println(rollNumber("Aman"))
+  println("rollNumberOld(Aman) "+ rollNumberOld("Aman"))
+
+  println("rollNumber(A) "+rollNumber("A"))
+
+  println("rollNumber(Aman) "+rollNumber("Aman"))
+
+  println("rollNo(Aman) "+rollNo("Aman"))
+
+  println("rollNo(A) "+rollNo("A"))
+
+  println(Try("1".toInt))
 
   println(rollNumberOld("A"))
 
